0fce6c9a20fefdbf771571853dfb55dbf3669695
haseslee <hadesleeq@gmail.com>
2015-09-30 14:10:09 +0900

    calculator TDD

--- src/main/java/OopAnotherExample.java
+++ src/main/java/OopAnotherExample.java
@@ -9,33 +9,65 @@
  */
 public class OopAnotherExample {
     public static void main(String[] args) {
-        final CalculatorService calculatorService = new CalculatorService();
-        final int additionResult = calculatorService.calculate('+', 1, 1);
+        final CalculatorService calculatorService = new CalculatorService(new Addition());
+        final int additionResult = calculatorService.calculate(1, 1);
         System.out.println(additionResult);
 
-        final int subtractionResult = calculatorService.calculate('-', 1, 1);
+        final int subtractionResult = calculatorService.calculate(1, 1);
         System.out.println(subtractionResult);
 
-        final int multiplicactionResult = calculatorService.calculate('*', 1, 1);
+        final int multiplicactionResult = calculatorService.calculate(1, 1);
         System.out.println(multiplicactionResult);
 
-        final int divisionResult = calculatorService.calculate('/', 8, 4);
+        final int divisionResult = calculatorService.calculate(8, 4);
         System.out.println(divisionResult);
     }
 }
 
+interface Calculation {
+    int calculate(final int num1, final int num2);
+}
+
+class Addition implements Calculation {
+
+    @Override
+    public int calculate(final int num1, final int num2) {
+        return num1 + num2;
+    }
+}
+
+class Subtraction implements Calculation {
+
+    @Override
+    public int calculate(final int num1, final int num2) {
+        return num1 - num2;
+    }
+}
+
+class Multiplication implements Calculation {
+
+    @Override
+    public int calculate(final int num1, final int num2) {
+        return num1 * num2;
+    }
+}
+
+class Division implements Calculation {
+
+    @Override
+    public int calculate(final int num1, final int num2) {
+        return num1 / num2;
+    }
+}
+
 class CalculatorService {
-    public int calculate(char calculation, int num1, int num2) {
-        if (calculation == '+') {
-            return num1 + num2;
-        } else if (calculation == '-') {
-            return num1 - num2;
-        } else if (calculation == '*') {
-            return num1 * num2;
-        } else if (calculation == '/') {
-            return num1 / num2;
-        } else {
-            throw new IllegalArgumentException("Unknown calculation: " + calculation);
-        }
+    private final Calculation calculation;
+
+    public CalculatorService(Calculation calculation) {
+        this.calculation = calculation;
+    }
+
+    public int calculate(int num1, int num2) {
+        return calculation.calculate(num1, num2);
     }
 }
--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -15,36 +16,36 @@ public class CalculatorServiceTest {
 
     @Test
     public void testCalculateAddition() throws Exception {
-        CalculatorService calculatorService = new CalculatorService();
+        Calculation calculation = new Addition();
 
-        final int actual = calculatorService.calculate('+', 1, 1);
+        final int actual = calculation.calculate(1, 1);
 
         assertThat(actual).isEqualTo(2);
     }
 
     @Test
     public void testCalculateSubtraction() throws Exception {
-        CalculatorService calculatorService = new CalculatorService();
+        Calculation calculation = new Subtraction();
 
-        final int actual = calculatorService.calculate('-', 1, 1);
+        final int actual = calculation.calculate(1, 1);
 
         assertThat(actual).isEqualTo(0);
     }
 
     @Test
     public void testCalculateMultiplication() throws Exception {
-        CalculatorService calculatorService = new CalculatorService();
+        Calculation calculation = new Multiplication();
 
-        final int actual = calculatorService.calculate('*', 1, 1);
+        final int actual = calculation.calculate(1, 1);
 
         assertThat(actual).isEqualTo(1);
     }
 
     @Test
     public void testCalculateDivision() throws Exception {
-        CalculatorService calculatorService = new CalculatorService();
+        Calculation calculation = new Division();
 
-        final int actual = calculatorService.calculate('/', 8, 4);
+        final int actual = calculation.calculate(8, 4);
 
         assertThat(actual).isEqualTo(2);
     }
