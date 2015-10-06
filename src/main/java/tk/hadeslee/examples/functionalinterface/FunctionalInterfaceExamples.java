2ed44184ad2bfd9627ae93118d88c67449141766
hadeslee <hadesleeq@gmail.com>
2015-10-06 15:18:44 +0900

    lombok Ex

--- src/main/java/tk/hadeslee/examples/functionalinterface/FunctionalInterfaceExamples.java
+++ src/main/java/tk/hadeslee/examples/functionalinterface/FunctionalInterfaceExamples.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,15 +14,24 @@ import java.util.List;
 public class FunctionalInterfaceExamples {
     public static void main(String[] args) {
         final List<Product> products = Arrays.asList(
-                new Product(1L,"A", new BigDecimal("10.00")),
-                new Product(2L,"B", new BigDecimal("55.50")),
-                new Product(3L,"C", new BigDecimal("17.45")),
-                new Product(4L,"D", new BigDecimal("23.00")),
-                new Product(5L,"E", new BigDecimal("110.99"))
+                new Product(1L, "A", new BigDecimal("10.00")),
+                new Product(2L, "B", new BigDecimal("55.50")),
+                new Product(3L, "C", new BigDecimal("17.45")),
+                new Product(4L, "D", new BigDecimal("23.00")),
+                new Product(5L, "E", new BigDecimal("110.99"))
         );
-        System.out.println(products);
+
+        final BigDecimal twenty = new BigDecimal("20");
+        final List<Product> result = new ArrayList<>();
+        for (final Product product : products) {
+            if (product.getPrice().compareTo(twenty) >= 0) {
+                result.add(product);
+            }
+        }
+        System.out.println(result);
     }
 }
+
 @AllArgsConstructor
 @Data
 class Product {
