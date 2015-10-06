0c045375689493eeb51e2a1f6b16698aba097637
hadeslee <hadesleeq@gmail.com>
2015-10-06 11:43:06 +0900

    @FunctionalInterface Ex3

--- src/main/java/tk/hadeslee/examples/java8/FunctionalInterfaceExamples.java
+++ src/main/java/tk/hadeslee/examples/java8/FunctionalInterfaceExamples.java
@@ -107,6 +107,9 @@ public class FunctionalInterfaceExamples {
     }
 }
 
+@FunctionalInterface
 interface Function3<T1, T2, T3, R> {
     R apply(T1 t1, T2 t2, T3 t3);
+
+//    void print(int i);
 }
