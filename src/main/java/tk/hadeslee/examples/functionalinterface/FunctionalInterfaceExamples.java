2218fedf6e1b6bce31142a9a65365c35ffb1f83c
hadeslee <hadesleeq@gmail.com>
2015-10-06 16:21:06 +0900

    필터를 이용한 람다 익스프레션

--- src/main/java/tk/hadeslee/examples/functionalinterface/FunctionalInterfaceExamples.java
+++ src/main/java/tk/hadeslee/examples/functionalinterface/FunctionalInterfaceExamples.java
@@ -2,11 +2,13 @@ package tk.hadeslee.examples.functionalinterface;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -23,13 +25,28 @@ public class FunctionalInterfaceExamples {
         );
 
         final BigDecimal twenty = new BigDecimal("20");
-        System.out.println("products >= $20: "+
-        filter(products, product -> product.getPrice().compareTo(twenty) >= 0
-        ));
+        System.out.println("products >= $20: " +
+                filter(products, product -> product.getPrice().compareTo(twenty) >= 0
+                ));
 
-        System.out.println("products <= $10: "+
+        System.out.println("products <= $10: " +
                 filter(products, product -> product.getPrice().compareTo(new BigDecimal("10")) <= 0
                 ));
+
+        final List<Product> expensiveProducts = filter(products, product -> product.getPrice().compareTo(new BigDecimal("50")) > 0);
+
+//        final List<DiscountedProduct> discountedProducts = new ArrayList<>();
+//        for (final Product product : expensiceProducts) {
+//            discountedProducts.add(new DiscountedProduct(product.getId(), product.getName(), product.getPrice()));
+//        }
+        final List<Product> discountedProducts =
+                map(expensiveProducts, product -> new DiscountedProduct(product.getId(), product.getName(), product.getPrice().multiply(new BigDecimal("0.5"))));
+        System.out.println("expensive products: " + expensiveProducts);
+        System.out.println("discounted products: " + discountedProducts);
+
+        System.out.println("discounted products (<= $30 " +
+                        filter(discountedProducts, product -> product.getPrice().compareTo(new BigDecimal("30")) <= 0)
+        );
     }
 
     private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
@@ -41,6 +58,14 @@ public class FunctionalInterfaceExamples {
         }
         return result;
     }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        final List<R> result = new ArrayList<>();
+        for (final T t : list) {
+            result.add(function.apply(t));
+        }
+        return result;
+    }
 }
 
 @AllArgsConstructor
@@ -49,4 +74,11 @@ class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+}
+
+@ToString(callSuper = true)
+class DiscountedProduct extends Product {
+    public DiscountedProduct(Long id, String name, BigDecimal price) {
+        super(id, name, price);
+    }
 }
