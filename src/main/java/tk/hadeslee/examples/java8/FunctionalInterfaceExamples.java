7809f49ddad5d953c7b49b3b8c752b27f025ed94
hadeslee <hadesleeq@gmail.com>
2015-10-06 11:34:52 +0900

    Functional Interface Ex

--- src/main/java/tk/hadeslee/examples/java8/FunctionalInterfaceExamples.java
+++ src/main/java/tk/hadeslee/examples/java8/FunctionalInterfaceExamples.java
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
  * Created by redfo on 2015-10-01.
  */
 public class FunctionalInterfaceExamples {
-    public static void main(String[] args) {
+    private void run4FunctionalInterfaces() {
         final Function<String, Integer> toInt = value -> Integer.parseInt(value);
 
         final Integer number = toInt.apply("100");
@@ -96,4 +96,16 @@ public class FunctionalInterfaceExamples {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        println("Area is  ", 12, 20, (message, length, width) -> message + (length * width));
+    }
+
+    private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function) {
+        System.out.println(function.apply(t1, t2, t3));
+    }
+}
+
+interface Function3<T1, T2, T3, R> {
+    R apply(T1 t1, T2 t2, T3 t3);
 }
