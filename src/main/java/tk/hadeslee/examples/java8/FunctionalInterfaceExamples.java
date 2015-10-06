2fd4091ffcaf8d296e00dc2ccfecf5fd6a4b7732
hadeslee <hadesleeq@gmail.com>
2015-10-06 11:39:54 +0900

    Functional Interface Ex2

--- src/main/java/tk/hadeslee/examples/java8/FunctionalInterfaceExamples.java
+++ src/main/java/tk/hadeslee/examples/java8/FunctionalInterfaceExamples.java
@@ -98,7 +98,8 @@ public class FunctionalInterfaceExamples {
     }
 
     public static void main(String[] args) {
-        println("Area is  ", 12, 20, (message, length, width) -> message + (length * width));
+        println(1L, "Hades", "test@email.com",
+                (id, name, email) -> "User info : ID=" + id + ", name=" + name + ", email=" + email);
     }
 
     private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function) {
