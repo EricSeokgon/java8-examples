ff66001c2ba951593a2134121fbeedb5af8a1915
hadeslee <hadesleeq@gmail.com>
2015-10-06 15:07:46 +0900

    lombok @Data

--- src/main/java/tk/hadeslee/examples/functionalinterface/FunctionalInterfaceExamples.java
+++ src/main/java/tk/hadeslee/examples/functionalinterface/FunctionalInterfaceExamples.java
@@ -1,7 +1,8 @@
 package tk.hadeslee.examples.functionalinterface;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
-import java.util.Objects;
 
 /**
  * Created by redfo on 2015-10-06.
@@ -11,66 +12,9 @@ public class FunctionalInterfaceExamples {
 
     }
 }
-
+@Data
 class Product {
     private Long id;
     private String name;
     private BigDecimal price;
-
-    public Product() {
-    }
-
-    public Product(Long id, String name, BigDecimal price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return new StringBuilder("Product{")
-                .append("id=").append(id)
-                .append(", name='").append(name).append('\'')
-                .append(", price=").append(price)
-                .append('}').toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Product)) return false;
-        Product product = (Product) o;
-        return Objects.equals(getId(), product.getId()) &&
-                Objects.equals(getName(), product.getName()) &&
-                Objects.equals(getPrice(), product.getPrice());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getPrice());
-    }
 }
