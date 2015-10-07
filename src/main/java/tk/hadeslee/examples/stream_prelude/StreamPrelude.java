268aa5b4d13ad3c1fa0e22888a8e433af5a21340
Hases Lee <hadesleeq@gmail.com>
2015-10-07 15:29:00 +0900

    Identity Function

--- src/main/java/tk/hadeslee/examples/stream_prelude/StreamPrelude.java
+++ src/main/java/tk/hadeslee/examples/stream_prelude/StreamPrelude.java
@@ -29,15 +29,39 @@ public class StreamPrelude {
         System.out.println("minInt: " + minInt);
 
         final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println("mapOld(numbers, i -> i * 2)");
         System.out.println(
+                mapOld(numbers, i -> i * 2)
+        );
+        System.out.println("mapOld(numbers, null)");
+        System.out.println(
+                mapOld(numbers, null)
+        );
+
+        System.out.println("\n");
+        System.out.println("map(numbers, i -> i * 2)\n"+
                 map(numbers, i -> i * 2)
         );
-        System.out.println(
+        System.out.println("map(numbers, null)\n"+
                 map(numbers, null)
         );
     }
-
     private static <T, R> List<R> map(final List<T> list, final Function<T, R> mapper) {
+        final Function<T, R> function;
+        if (mapper != null) {
+            function = mapper;
+        } else {
+            function = t -> (R) t;
+        }
+
+        final List<R> result = new ArrayList<>();
+        for (final T t : list) {
+            result.add(function.apply(t));
+        }
+        return result;
+    }
+
+    private static <T, R> List<R> mapOld(final List<T> list, final Function<T, R> mapper) {
         final List<R> result;
         if (mapper != null) {
             result = new ArrayList<>();
