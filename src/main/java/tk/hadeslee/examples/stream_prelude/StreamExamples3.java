6eb3d53af2e358b8186d5e1afc3137a75998527b
Hadeslee <leesk55@gmail.com>
2015-10-09 00:37:00 +0900

    Stream toSet

--- src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
+++ src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
@@ -3,6 +3,7 @@ package tk.hadeslee.examples.stream_prelude;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 /**
  * Project: java8-examples
@@ -15,12 +16,20 @@ import static java.util.stream.Collectors.toList;
  */
 public class StreamExamples3 {
     public static void main(String[] args) {
-        System.out.println("collect(toList())" +
+        System.out.println("collect(toList()): " +
                         Stream.of(1, 2, 3, 4, 5)
                                 .filter(i -> i > 2)
                                 .map(i -> i * 2)
                                 .map(i -> "#" + i)
                                 .collect(toList())
+        );
+
+        System.out.println("collect(toSet()): " +
+                        Stream.of(1, 2, 3, 4, 5)
+                                .filter(i -> i > 2)
+                                .map(i -> i * 2)
+                                .map(i -> "#" + i)
+                                .collect(toSet())
         );
     }
 }
