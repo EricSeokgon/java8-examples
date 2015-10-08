b536528bc945d52a23391f3c919ed868c2e0f4ba
Hadeslee <leesk55@gmail.com>
2015-10-09 00:36:04 +0900

    Stream toList

---
+++ src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
@@ -0,0 +1,26 @@
+package tk.hadeslee.examples.stream_prelude;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Project: java8-examples
+ * FileName: StreamExamples3
+ * Date: 2015-10-09
+ * Time: 오전 12:29
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class StreamExamples3 {
+    public static void main(String[] args) {
+        System.out.println("collect(toList())" +
+                        Stream.of(1, 2, 3, 4, 5)
+                                .filter(i -> i > 2)
+                                .map(i -> i * 2)
+                                .map(i -> "#" + i)
+                                .collect(toList())
+        );
+    }
+}
