d72046d4873a78888300757bf2c6aa43e166ed16
Hadeslee <leesk55@gmail.com>
2015-10-10 08:01:03 +0900

    collect joining

--- src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
+++ src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
@@ -2,6 +2,7 @@ package tk.hadeslee.examples.stream_prelude;
 
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
@@ -25,11 +26,37 @@ public class StreamExamples3 {
         );
 
         System.out.println("collect(toSet()): " +
-                        Stream.of(1, 2, 3, 4, 5)
+                        Stream.of(1, 3, 3, 5, 5)
                                 .filter(i -> i > 2)
                                 .map(i -> i * 2)
                                 .map(i -> "#" + i)
                                 .collect(toSet())
         );
+
+        System.out.println("collect(joining()): " +
+                        Stream.of(1, 3, 3, 5, 5)
+                                .filter(i -> i > 2)
+                                .map(i -> i * 2)
+                                .map(i -> "#" + i)
+                                .collect(joining())
+        );
+
+        System.out.println("collect(joining(\", \")): " +
+                        Stream.of(1, 3, 3, 5, 5)
+                                .filter(i -> i > 2)
+                                .map(i -> i * 2)
+                                .map(i -> "#" + i)
+                                .collect(joining(", "))
+        );
+
+        System.out.println("collect(joining(\", \", \"[\", \"]\")): " +
+                        Stream.of(1, 3, 3, 5, 5)
+                                .filter(i -> i > 2)
+                                .map(i -> i * 2)
+                                .map(i -> "#" + i)
+                                .collect(joining(", ","[", "]")) //[#6, #6, #10, #10]
+        );
+
+
     }
 }
