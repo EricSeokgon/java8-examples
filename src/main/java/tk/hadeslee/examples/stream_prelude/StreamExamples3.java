6640be865b880b4313e90cde211236fe4acba217
Hases Lee <hadesleeq@gmail.com>
2015-10-14 09:56:29 +0900

    forEach Stream

--- src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
+++ src/main/java/tk/hadeslee/examples/stream_prelude/StreamExamples3.java
@@ -1,10 +1,10 @@
 package tk.hadeslee.examples.stream_prelude;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.*;
 
 /**
  * Project: java8-examples
@@ -101,6 +101,20 @@ public class StreamExamples3 {
                         .filter(i -> i.equals(integer128))
                         .findFirst()
         );
+
+        System.out.println(".filter(i -> i > integer3).count()" +
+                        Stream.of(1, 2, 3, 4, 5)
+                                .filter(i -> i > integer3)
+                                .count()
+        );
+
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        for (Integer i : numbers) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("forEach(i -> System.out.println(i))");
+        Stream.of(1, 2, 3, 4, 5)
+                .forEach(i -> System.out.println(i));
 
     }
 }
