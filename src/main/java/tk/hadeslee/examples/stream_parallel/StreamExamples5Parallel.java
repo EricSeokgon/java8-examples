31b8cd9df0d9b6dcb23f2c2565dbc4fb0c0c0a6a
Hases Lee <hadesleeq@gmail.com>
2015-10-16 10:12:34 +0900

    Parallel Stream

--- src/main/java/tk/hadeslee/examples/stream_parallel/StreamExamples5Parallel.java
+++ src/main/java/tk/hadeslee/examples/stream_parallel/StreamExamples5Parallel.java
@@ -1,5 +1,7 @@
 package tk.hadeslee.examples.stream_parallel;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 /**
@@ -17,21 +19,52 @@ public class StreamExamples5Parallel {
         final int[] sum = {0};
         IntStream.range(0, 100)
                 .forEach(i -> sum[0] += i);
-        System.out.println("         sum(with side-effect): " + sum[0]);
+        System.out.println("                   sum(side-effect): " + sum[0]);
 
         final int[] sum2 = {0};
         IntStream.range(0, 100)
                 .parallel()
                 .forEach(i -> sum2[0] += i);
-        System.out.println("parallel sum(with side-effect): " + sum2[0]);
 
-        System.out.println("    stream sum(no side-effect): " +
+        System.out.println("          parallel sum(side-effect): " + sum2[0]);
+
+        System.out.println("         stream sum(no side-effect): " +
                 IntStream.range(0, 100)
                         .sum());
-        System.out.println("    stream sum(no side-effect): " +
-                        IntStream.range(0, 100)
-                                .parallel()
-                                .sum()
-        );
+        System.out.println("parallel stream sum(no side-effect): " +
+                IntStream.range(0, 100)
+                        .parallel()
+                        .sum());
+        System.out.println("\n=================================");
+        System.out.println("Stream");
+        final long start = System.currentTimeMillis();
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+                .stream()
+                .map(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return i;
+                })
+                .forEach(i -> System.out.println(i));
+        System.out.println(System.currentTimeMillis() - start);
+
+        System.out.println("\n=================================");
+        System.out.println("Parallel Stream");
+        final long start2 = System.currentTimeMillis();
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+                .parallelStream()
+                .map(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return i;
+                })
+                .forEach(i -> System.out.println(i));
+        System.out.println(System.currentTimeMillis() - start2);
     }
 }
