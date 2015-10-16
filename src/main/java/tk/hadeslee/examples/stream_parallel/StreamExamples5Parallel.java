4e2a174ad41c999a8273d6e8903878c8e05ec923
Hases Lee <hadesleeq@gmail.com>
2015-10-16 09:59:47 +0900

    parallel sum(with side-effect)

--- src/main/java/tk/hadeslee/examples/stream_parallel/StreamExamples5Parallel.java
+++ src/main/java/tk/hadeslee/examples/stream_parallel/StreamExamples5Parallel.java
@@ -17,6 +17,21 @@ public class StreamExamples5Parallel {
         final int[] sum = {0};
         IntStream.range(0, 100)
                 .forEach(i -> sum[0] += i);
-        System.out.println("sum: " + sum[0]);
+        System.out.println("         sum(with side-effect): " + sum[0]);
+
+        final int[] sum2 = {0};
+        IntStream.range(0, 100)
+                .parallel()
+                .forEach(i -> sum2[0] += i);
+        System.out.println("parallel sum(with side-effect): " + sum2[0]);
+
+        System.out.println("    stream sum(no side-effect): " +
+                IntStream.range(0, 100)
+                        .sum());
+        System.out.println("    stream sum(no side-effect): " +
+                        IntStream.range(0, 100)
+                                .parallel()
+                                .sum()
+        );
     }
 }
