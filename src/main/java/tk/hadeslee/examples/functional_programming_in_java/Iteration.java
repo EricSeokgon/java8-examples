c686ad6d660004ae9604536c06608dc09c47546b
hadesyi <hadesleeq@gmail.ccom>
2015-10-18 00:28:41 +0900

    iterating through a List

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Iteration.java
@@ -0,0 +1,25 @@
+package tk.hadeslee.examples.functional_programming_in_java;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: Iteration
+ * Date: 2015-10-18
+ * Time: 오전 12:24
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Iteration {
+    public static void main(String[] args) {
+        final List<String> friends = Arrays.asList("Brian", "nate", "neal", "Raju", "Sara", "Scott");
+        for (int i = 0; i < friends.size(); i++) {
+            System.out.println(friends.get(i));
+
+        }
+
+    }
+}
+
