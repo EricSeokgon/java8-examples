11bb6764d9035a2f442ab0ae47df1845bc0dc84e
hadesyi <hadesleeq@gmail.com>
2015-10-20 11:11:51 +0900

    Lambda Examples

--- src/main/java/tk/hadeslee/examples/closure/ClosureExamples.java
+++ src/main/java/tk/hadeslee/examples/closure/ClosureExamples.java
@@ -13,12 +13,18 @@ public class ClosureExamples {
     public static void main(String[] args) {
         final int number = 100;
 
-        final Runnable runnable = new Runnable() {
+        testClosure("Anonymous Class", new Runnable() {
             @Override
             public void run() {
                 System.out.println(number);
             }
-        };
+        });
+
+        testClosure("Lambda Expression", () -> System.out.println(number));
+    }
+
+    private static void testClosure(final String name, final Runnable runnable) {
+        System.out.println("Start: " + name + " : ");
         runnable.run();
     }
 }
