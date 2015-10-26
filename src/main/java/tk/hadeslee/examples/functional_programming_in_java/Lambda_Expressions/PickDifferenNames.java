c8a174c5d07f309f528163e19fe95d004756f4f1
hadesyi <hadesleeq@gmail.com>
2015-10-26 09:46:06 +0900

    Duplication in Lambda Expressions

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/PickDifferenNames.java
@@ -0,0 +1,29 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Project: java8-examples
+ * FileName: PickDifferenNames
+ * Date: 2015-10-26
+ * Time: 오전 9:43
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class PickDifferenNames {
+    public static void main(String[] args) {
+        final List<String> friends = Arrays.asList("Brian", "nate", "neal", "Raju", "Sara", "Scott");
+        final Predicate<String> startsWithN = name -> name.startsWith("N");
+        final Predicate<String> startsWithB = name -> name.startsWith("B");
+
+        final long countFriendsStartN =
+                friends.stream().filter(startsWithN).count();
+
+        final long countFriendsStartB =
+                friends.stream().filter(startsWithB).count();
+
+    }
+}
