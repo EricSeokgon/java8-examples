1cc6f29ab53917b60ac34f46693bddada8761bd7
hadesyi <hadesleeq@gmail.com>
2015-10-27 11:44:34 +0900

    Picking an Element lambda expressions

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/PickAnElementelegant.java
@@ -0,0 +1,26 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Project: java8-examples
+ * FileName: PickAnElementelegant
+ * Date: 2015-10-27
+ * Time: 오전 11:41
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class PickAnElementelegant {
+    public static void pickName(
+            final List<String> names, final String startingLetter) {
+
+        final Optional<String> foundName =
+                names.stream()
+                        .filter(name -> name.startsWith(startingLetter))
+                        .findFirst();
+
+        System.out.println(String.format("A name starting with %s: %s", startingLetter, foundName.orElse("No name found")));
+    }
+}
