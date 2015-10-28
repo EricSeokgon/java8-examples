807afd27a60fc3c204303bcef8748d2025ffe459
hadesyi <hadesleeq@gmail.com>
2015-10-28 10:02:36 +0900

    lambda expression reduce() method

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/PickALongest.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/PickALongest.java
@@ -2,6 +2,8 @@ package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 
 /**
  * Project: java8-examples
@@ -16,9 +18,12 @@ public class PickALongest {
     public static void main(String[] args) {
         final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
 
-        System.out.println("Total number od characters in all names: " +
+        final Optional<String> aLongName =
                 friends.stream()
-                        .mapToInt(name -> name.length())
-                        .sum());
+                        .reduce((name1, name2) ->
+                                name1.length() >= name2.length() ? name1 : name2);
+        aLongName.ifPresent(name ->
+                System.out.println(String.format("A longest name: %s", name)));
+
     }
 }
---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/friends.java
@@ -0,0 +1,17 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: friends
+ * Date: 2015-10-28
+ * Time: 오전 9:47
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class friends {
+    final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+}
