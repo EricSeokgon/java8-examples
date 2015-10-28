8caf15c720b57169aaf9a6f326aeaff40637cc14
hadesyi <hadesleeq@gmail.com>
2015-10-29 01:46:59 +0900

    Folks frameworks

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Folks.java
@@ -0,0 +1,19 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: Folks
+ * Date: 2015-10-29
+ * Time: 오전 1:45
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Folks {
+    final public static List<String> friends = Arrays.asList("Brian", "nate", "neal", "Raju", "Sara", "Scott");
+    final public static List<String> comrades = Arrays.asList("Kate", "Ken", "Nick", "Paula", "Zach");
+    final public static List<String> editors = Arrays.asList("Brian", "Jackie", "Jogh", "Mike");
+}
