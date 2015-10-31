2257b74673a2d98d2c95cab64618e20b02931794
hadesyi <hadesleeq@gmail.com>
2015-11-01 00:31:19 +0900

    Comparator interface

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/Compare.java
@@ -0,0 +1,24 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.String_Compararors_Filters;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: Compare
+ * Date: 2015-11-01
+ * Time: 오전 12:29
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Compare {
+    final List<Person> people = Arrays.asList(
+            new Person("John", 20),
+            new Person("Sara", 21),
+            new Person("Jane", 21),
+            new Person("Greg", 35)
+
+    );
+
+}
