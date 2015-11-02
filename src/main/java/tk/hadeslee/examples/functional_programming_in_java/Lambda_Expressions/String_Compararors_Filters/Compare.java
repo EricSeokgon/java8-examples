151fb0c650a75de3559ac5827118ecb0463ea8e6
hadesyi <hadesleeq@gmail.com>
2015-11-02 10:42:26 +0900

    Reusing a Comparator

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/Compare.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/Compare.java
@@ -1,8 +1,9 @@
 package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.String_Compararors_Filters;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -23,12 +24,48 @@ public class Compare {
                 new Person("Jane", 21),
                 new Person("Greg", 35));
 
+        Comparator<Person> compareAscending = (person1, person2) ->
+                person1.ageDifference(person2);
+        Comparator<Person> compareDescending = compareAscending.reversed();
+
         List<Person> ascendingAge =
                 people.stream()
                         .sorted((person1, person2) -> person1.ageDifference(person2))
                         .collect(toList());
-        printPeople("Sorted in ascenfing order of age: ", ascendingAge);
+        printPeople("Sorted in ascending order of age: ", ascendingAge);
+        System.out.println();
+
+        printPeople("Sorted in descending order of age: ",
+                people.stream()
+                        .sorted((person1, person2) -> person2.ageDifference(person1))
+                        .collect(toList()));
+
+        printPeople("Sorted in ascending order of age: ",
+                people.stream()
+                        .sorted(compareAscending)
+                        .collect(toList()));
+
+        printPeople("Sorted in descending order of age: ",
+                people.stream()
+                        .sorted(compareDescending)
+                        .collect(toList()));
+
+        printPeople("Sorted in ascending order of name: ",
+                people.stream()
+                        .sorted((person1, person2) ->
+                                person1.getName().compareTo(person2.getName()))
+                        .collect(toList()));
+
+        people.stream()
+                .min(Person::ageDifference)
+                .ifPresent(yungest -> System.out.println("Youngest: " + yungest));
+
+        people.stream()
+                .max(Person::ageDifference)
+                .ifPresent(eldest -> System.out.println("Eldest: " + eldest));
+
     }
+
 
     public static void printPeople(
             final String message, final List<Person> people) {
