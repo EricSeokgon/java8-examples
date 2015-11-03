d500eff7a5ba298d975e7aa02fbe45e6bec2c830
hadesyi <hadesleeq@gmail.com>
2015-11-03 10:34:48 +0900

    Let's use groupingBy() to groups people by their age.

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/OlderThan20.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/OlderThan20.java
@@ -4,6 +4,8 @@ import static tk.hadeslee.examples.functional_programming_in_java.Lambda_Express
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Project: java8-examples
@@ -24,10 +26,22 @@ public class OlderThan20 {
         System.out.println("People older than 20 (forEach): " + olderThan20_forEach);
 
         List<Person> olderThan20_collect =
-        people.stream()
-                .filter(person -> person.getAge() > 20)
-                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+                people.stream()
+                        .filter(person -> person.getAge() > 20)
+                        .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
         System.out.println("People older than 20 (collect): " + olderThan20_collect);
+
+        List<Person> olderThan20_collect_concise =
+                people.stream()
+                        .filter(person -> person.getAge() > 20)
+                        .collect(Collectors.toList());
+        System.out.println("People older than 20 (collect_concise): " + olderThan20_collect_concise);
+
+        Map<Integer, List<Person>> peopleByAge =
+                people.stream()
+                        .collect(Collectors.groupingBy(Person::getAge));
+        System.out.println("People grouped by age: " + peopleByAge);
+
 
     }
 
