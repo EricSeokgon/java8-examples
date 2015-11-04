0301577044f8f27ee20ef2ee4371f8303c15e542
hadesyi <hadesleeq@gmail.com>
2015-11-04 09:42:49 +0900

    in the current directory instead of a listing of all the files, useing the filter() method

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/Listfiles.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/Listfiles.java
@@ -16,6 +16,7 @@ import java.nio.file.Paths;
 public class Listfiles {
     public static void main(String[] args) throws IOException {
         Files.list(Paths.get("."))
+                .filter(Files::isDirectory)
                 .forEach(System.out::println);
     }
 }
