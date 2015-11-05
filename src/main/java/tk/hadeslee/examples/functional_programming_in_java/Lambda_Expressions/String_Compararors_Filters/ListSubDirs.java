78b28918caa6a1ad9d856a5c6afafd8352dc5b6b
hadesyi <hadesleeq@gmail.com>
2015-11-05 09:19:56 +0900

    Let's use the traditional for loop first to iterate over the files in a given directory.

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/String_Compararors_Filters/ListSubDirs.java
@@ -0,0 +1,32 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.String_Compararors_Filters;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: ListSubDirs
+ * Date: 2015-11-05
+ * Time: 오전 9:14
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ListSubDirs {
+    public static void main(String[] args) {
+        List<File> files = new ArrayList<>();
+
+        File[] filesInCurerentDir = new File(".").listFiles();
+        for (File file : filesInCurerentDir) {
+            File[] filesInSubDir = file.listFiles();
+            if (filesInSubDir != null) {
+                files.addAll(Arrays.asList(filesInSubDir));
+            } else {
+                files.add(file);
+            }
+        }
+        System.out.println("Count: " + files.size());
+    }
+}
