e2a7d0482e85adbb8393901e94b38a26e3a7a03d
hadesyi <hadesleeq@gmail.com>
2015-11-13 00:35:03 +0900

    Designing Filters

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Designing_with_Lambda_Expreesions/Camera.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Designing_with_Lambda_Expreesions/Camera.java
@@ -1,6 +1,8 @@
 package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Designing_with_Lambda_Expreesions;
 
 import java.awt.*;
+import java.util.Arrays;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -20,5 +22,35 @@ public class Camera {
         final Color processedColor = filter.apply(inputcolor);
         return processedColor;
     }
+
+    public void setFilters(final Function<Color, Color>... filters) {
+        filter =
+                Arrays.asList(filters).stream()
+                        .reduce((filter, next) -> filter.compose(next))
+                        .orElse(color -> color);
+    }
+
+    public Camera() {
+        setFilters();
+    }
+
+
+    public static void main(String[] args) {
+        final Camera camera = new Camera();
+        final Consumer<String> printCaptured = (filterInfo) ->
+                System.out.println(String.format("with %s: %s", filterInfo,
+                        camera.capture((new Color(200, 100, 200)))));
+        printCaptured.accept("no filter");
+
+        camera.setFilters(Color::brighter);
+        printCaptured.accept("brighter filter");
+
+        camera.setFilters(Color::darker);
+        printCaptured.accept("darker filter");
+
+        camera.setFilters(Color::brighter, Color::darker);
+        printCaptured.accept("brighter & darker filter");
+    }
+
 
 }
