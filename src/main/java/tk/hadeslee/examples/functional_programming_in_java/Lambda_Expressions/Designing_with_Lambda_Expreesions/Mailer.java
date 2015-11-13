4bd97518e84f1591ca97c8f2d5f15fe76034b929
hadesyi <hadesleeq@gmail.com>
2015-11-14 00:17:34 +0900

    Starting with a Design

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Designing_with_Lambda_Expreesions/Mailer.java
@@ -0,0 +1,18 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Designing_with_Lambda_Expreesions;
+
+/**
+ * Project: java8-examples
+ * FileName: Mailer
+ * Date: 2015-11-14
+ * Time: 오전 12:14
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mailer {
+    public void from(final String address){}
+    public void to(final String address){}
+    public void subject(final String line){}
+    public void body(final String message){}
+    public void send(){System.out.println("sending...");}
+}
