14c88c301d514322bdbf3879dc41c6d7e5b89ae9
hadesyi <hadesleeq@gmail.com>
2015-11-16 09:35:47 +0900

    Making the API Intuitive and Fluent

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Designing_with_Lambda_Expreesions/FluentMailer.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Designing_with_Lambda_Expreesions/FluentMailer.java
@@ -12,16 +12,39 @@ import java.util.function.Consumer;
  * To change this template use File | Settings | File Templates.
  */
 public class FluentMailer {
-    private FluentMailer(){}
-    public FluentMailer from(final String address){return this;}
-    public FluentMailer to(final String address){return this;}
-    public FluentMailer subject(final String line){return this;}
-    public FluentMailer body(final String message){return this;}
+    private FluentMailer() {
+    }
 
-    public static void send(final Consumer<FluentMailer> block){
+    public FluentMailer from(final String address) {
+        return this;
+    }
+
+    public FluentMailer to(final String address) {
+        return this;
+    }
+
+    public FluentMailer subject(final String line) {
+        return this;
+    }
+
+    public FluentMailer body(final String message) {
+        return this;
+    }
+
+    public static void send(final Consumer<FluentMailer> block) {
         final FluentMailer mailer = new FluentMailer();
         block.accept(mailer);
         System.out.println("sending ...");
     }
+
+    public static void main(String[] args) {
+        FluentMailer.send(mailer ->
+                mailer.from("sklee@dbcore.com")
+                        .to("sklee@dbcore.com")
+                        .subject("build notification")
+                        .body("...much better..."));
+
+    }
+
 
 }
