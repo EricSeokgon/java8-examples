ca4f444c055823fdb2a39d5f7317e7643480ff31
hadesyi <hadesleeq@gmail.com>
2015-11-20 09:26:53 +0900

    Preparing the Class for Resource Cleanup

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Working_with_Resources/FileWriterEAM.java
@@ -0,0 +1,30 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Working_with_Resources;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Project: java8-examples
+ * FileName: FileWriterEAM
+ * Date: 2015-11-20
+ * Time: 오전 9:24
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileWriterEAM {
+    private final FileWriter writer;
+
+    private FileWriterEAM(final String fileName) throws IOException {
+        writer = new FileWriter(fileName);
+    }
+
+    private void close() throws IOException {
+        System.out.println("close called automatically...");
+        writer.close();
+    }
+
+    public void writeStuff(final String message) throws IOException {
+        writer.write(message);
+    }
+}
