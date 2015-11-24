2e4513515e97a78645ef3ef27c3e70f776c16db9
hadesyi <hadesleeq@gmail.com>
2015-11-24 10:45:39 +0900

    Adding a Level of Indirection

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Holder.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Holder.java
@@ -15,8 +15,21 @@ public class Holder {
     public Holder(){
         System.out.println("Holder created");
     }
-
-    public Heavy getHeavy() {
+    public Heavy getHeavy(){
         return heavy.get();
     }
+
+
+    private synchronized Heavy createAbdCacheHeavy(){
+        class HeavyFactory implements Supplier<Heavy>{
+            private final Heavy heavyInstance = new Heavy();
+
+            public Heavy get() { return heavyInstance; }
+         }
+        if (!HeavyFactory.class.isInstance(heavy)){
+            heavy = new HeavyFactory();
+        }
+        return heavy.get();
+    }
+
 }
---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Supplier.java
@@ -0,0 +1,14 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Being_lazy;
+
+/**
+ * Project: java8-examples
+ * FileName: Supplier
+ * Date: 2015-11-24
+ * Time: 오전 10:38
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Supplier<T> {
+    Heavy get();
+}
