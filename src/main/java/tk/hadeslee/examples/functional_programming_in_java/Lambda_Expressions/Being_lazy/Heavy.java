2e228a3241407314ac4ac928c374973ac09f82ce
hadesyi <hadesleeq@gmail.com>
2015-11-24 10:15:09 +0900

    create the heavy class

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Heavy.java
@@ -0,0 +1,20 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Being_lazy;
+
+/**
+ * Project: java8-examples
+ * FileName: Heavy
+ * Date: 2015-11-24
+ * Time: 오전 10:13
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Heavy {
+    public Heavy() {
+        System.out.println("Heacy created");
+    }
+
+    public String toString() {
+        return "quit heavy ";
+    }
+}
