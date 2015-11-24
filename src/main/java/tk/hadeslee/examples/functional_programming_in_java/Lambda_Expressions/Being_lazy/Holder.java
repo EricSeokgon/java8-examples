a7705d4277a787acf8dc135315e614ab02272d7d
hadesyi <hadesleeq@gmail.com>
2015-11-24 10:34:02 +0900

    Adding a Level of Indirection

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Holder.java
@@ -0,0 +1,22 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Being_lazy;
+
+/**
+ * Project: java8-examples
+ * FileName: Holder
+ * Date: 2015-11-24
+ * Time: 오전 10:31
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Holder {
+    private Supplier<Heavy> heavy = () -> createAbdCacheHeavy();
+
+    public Holder(){
+        System.out.println("Holder created");
+    }
+
+    public Heavy getHeavy() {
+        return heavy.get();
+    }
+}
