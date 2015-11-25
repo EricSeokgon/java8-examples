966492ecdd25b19c598c16a873fa16c5f365c5ae
hadesyi <hadesleeq@gmail.com>
2015-11-25 10:05:11 +0900

    Starting with Eager Evaluation

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Evaluation.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Being_lazy/Evaluation.java
@@ -15,4 +15,18 @@ public class Evaluation {
         simulateTimeConsumingOp(2000);
         return value > 100;
     }
+
+    private static void simulateTimeConsumingOp(int i) {
+
+    }
+
+    public static void eagerEvaluator(
+            final boolean input1, final boolean input2) {
+        System.out.println("eagerEvaluator called...");
+        System.out.println("accept?: " + (input1 && input2));
+    }
+
+    public static void main(String[] args) {
+        eagerEvaluator(evaluate(1), evaluate(2));
+    }
 }
