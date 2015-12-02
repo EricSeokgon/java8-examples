9cd9db78e2f2913ec3a52183ec842139f76fb6ca
hadesyi <hadesleeq@gmail.com>
2015-12-02 10:59:30 +0900

    an Optimization Problem

---
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Optimizing_recursions/RodCutterBasic.java
@@ -0,0 +1,20 @@
+package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Optimizing_recursions;
+
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: java
+ * Date: 2015-12-02
+ * Time: 오전 10:57
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class RodCutterBasic {
+    final List<Integer> prices;
+
+    public RodCutterBasic(List<Integer> pricesForLength) {
+        this.prices = pricesForLength;
+    }
+}
