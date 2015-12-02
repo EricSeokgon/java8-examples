9e72b6ed82e4ae5e5479aed114c379fb8983ea1c
hadesyi <hadesleeq@gmail.com>
2015-12-02 13:48:05 +0900

    Calculator class add

---
+++ src/main/java/tk/hadeslee/junit/Calculator.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.junit;
+
+/**
+ * Project: java8-examples
+ * FileName: Calculator
+ * Date: 2015-12-02
+ * Time: 오후 1:46
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+    public double add(double number1, double number2) {
+        return number1 + number2;
+    }
+}
