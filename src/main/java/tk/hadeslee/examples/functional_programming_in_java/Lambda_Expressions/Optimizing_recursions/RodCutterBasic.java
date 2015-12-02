237314da06689fdc758678d0139fd7c92e056091
hadesyi <hadesleeq@gmail.com>
2015-12-02 11:09:10 +0900

    Plain-Vanilla Recursion

--- src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Optimizing_recursions/RodCutterBasic.java
+++ src/main/java/tk/hadeslee/examples/functional_programming_in_java/Lambda_Expressions/Optimizing_recursions/RodCutterBasic.java
@@ -1,5 +1,6 @@
 package tk.hadeslee.examples.functional_programming_in_java.Lambda_Expressions.Optimizing_recursions;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,5 +17,22 @@ public class RodCutterBasic {
 
     public RodCutterBasic(List<Integer> pricesForLength) {
         this.prices = pricesForLength;
+    }
+
+    public int maxProfit(final int length) {
+        int profit = (length <= prices.size()) ? prices.get(length - 1) : 0;
+        for (int i = 1; i < length; i++) {
+            int priceWhenCut = maxProfit(i) + maxProfit(length - i);
+            if (profit < priceWhenCut) profit = priceWhenCut;
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        final List<Integer> priceValues = Arrays.asList(2, 1, 1, 2, 2, 2, 1, 8, 9, 15);
+        final RodCutterBasic rodCutter = new RodCutterBasic(priceValues);
+
+        System.out.println(rodCutter.maxProfit(5));
+        System.out.println(rodCutter.maxProfit(22));
     }
 }
