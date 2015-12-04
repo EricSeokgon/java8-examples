60914396f272fa6627d0eb8521e6f9987bf2496a
hadesyi <hadesleeq@gmail.com>
2015-12-04 10:35:57 +0900

    Using Function Composition

---
+++ src/main/java/tk/hadeslee/Lambda_Expressions/Composing_with_Lambda_Expressions/Tickers.java
@@ -0,0 +1,21 @@
+package tk.hadeslee.Lambda_Expressions.Composing_with_Lambda_Expressions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: java8-examples
+ * FileName: Tickers
+ * Date: 2015-12-04
+ * Time: 오전 10:32
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tickers {
+    public static final List<String> symbols = Arrays.asList(
+            "AMD", "HPQ", "IBM", "TXN", "VMW", "XRX", "APPL", "ADBE",
+            "AMZN", "CRAY", "CSCO", "DELL", "GOOG", "INTC", "INTU",
+            "MSFT", "ORCL", "TIBX", "VRSN", "YHOO"
+    );
+}
