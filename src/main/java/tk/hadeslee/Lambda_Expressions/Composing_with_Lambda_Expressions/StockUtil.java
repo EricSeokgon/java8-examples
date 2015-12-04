afa1d064a8dedc657b2634796298da33d94b2112
hadesyi <hadesleeq@gmail.com>
2015-12-05 00:51:00 +0900

    Preparing the Computations

--- src/main/java/tk/hadeslee/Lambda_Expressions/Composing_with_Lambda_Expressions/StockUtil.java
+++ src/main/java/tk/hadeslee/Lambda_Expressions/Composing_with_Lambda_Expressions/StockUtil.java
@@ -1,5 +1,8 @@
 package tk.hadeslee.Lambda_Expressions.Composing_with_Lambda_Expressions;
 
+import java.math.BigDecimal;
+import java.util.function.Predicate;
+
 /**
  * Project: java8-examples
  * FileName: StockUtil
@@ -13,4 +16,14 @@ public class StockUtil {
     public static StockInfo getPrice(final String ticker) {
         return new StockInfo(ticker, YahooFinance.getPrice(ticker));
     }
+
+    public static Predicate<StockInfo> isPriceLessThan(final int price) {
+        return stockInfo -> stockInfo.price.compareTo(BigDecimal.valueOf(price)) < 0;
+    }
+
+    public static StockInfo pickHigh(
+            final StockInfo stockInfo1, final StockInfo stockInfo2) {
+        return stockInfo1.price.compareTo(stockInfo2.price) > 0 ? stockInfo1 : stockInfo2;
+    }
+
 }
