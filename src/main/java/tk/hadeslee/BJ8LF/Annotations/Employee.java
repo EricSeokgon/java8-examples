fb9576ef67741784138a0d22016325b3c813df68
hadesyi <hadesleeq@gmail.com>
2015-12-09 01:24:12 +0900

    Employee class add

---
+++ src/main/java/tk/hadeslee/BJ8LF/Annotations/Employee.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: Employee
+ * Date: 2015-12-09
+ * Time: 오전 1:22
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+    public void setSalary(double salary) {
+        System.out.println("Employee.setSalary():" + salary);
+    }
+}
