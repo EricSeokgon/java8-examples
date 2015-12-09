42fc1f0edd2fe93b9f767e40d1e7f1c23a91f926
hadesyi <hadesleeq@gmail.com>
2015-12-09 10:48:06 +0900

    The Declaration of an Annotation Type Named Version

---
+++ src/main/java/tk/hadeslee/BJ8LF/Annotations/Version.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: Version
+ * Date: 2015-12-09
+ * Time: 오전 10:45
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface Version {
+    int major();
+
+    int minor();
+}
