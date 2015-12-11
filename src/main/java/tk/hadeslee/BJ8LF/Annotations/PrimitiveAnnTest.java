74f5698aa66196a38b7f0e79ce6e0bcd928ecd7e
hadesyi <hadesleeq@gmail.com>
2015-12-11 09:57:05 +0900

    Primitive Type

---
+++ src/main/java/tk/hadeslee/BJ8LF/Annotations/PrimitiveAnnTest.java
@@ -0,0 +1,28 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: PrimitiveAnnTest
+ * Date: 2015-12-11
+ * Time: 오전 9:54
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface PrimitiveAnnTest {
+    byte a();
+
+    short b();
+
+    int c();
+
+    long d();
+
+    float e();
+
+    double f();
+
+    boolean g();
+
+    char h();
+}
