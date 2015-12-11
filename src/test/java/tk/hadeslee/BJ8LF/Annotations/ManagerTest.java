49495f954c3adcf3328ca8907ab3fa8eac007c3b
hadesyi <hadesleeq@gmail.com>
2015-12-11 10:56:40 +0900

    Manager TestCase

---
+++ src/test/java/tk/hadeslee/BJ8LF/Annotations/ManagerTest.java
@@ -0,0 +1,23 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+import junit.framework.TestCase;
+
+/**
+ * Project: java8-examples
+ * FileName: ManagerTest
+ * Date: 2015-12-11
+ * Time: 오전 10:56
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ManagerTest extends TestCase {
+
+    public void testSetSalary() throws Exception {
+
+    }
+
+    public void testMain() throws Exception {
+
+    }
+}
