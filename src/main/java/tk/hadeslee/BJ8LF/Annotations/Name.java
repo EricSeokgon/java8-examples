60bc6b3f706b3f8e7ad04ac2d3ab4227975ce13a
hadesyi <hadesleeq@gmail.com>
2015-12-11 09:57:05 +0900

    String Type

---
+++ src/main/java/tk/hadeslee/BJ8LF/Annotations/Name.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: Name
+ * Date: 2015-12-11
+ * Time: 오전 10:05
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface Name {
+    String first();
+
+    String last();
+}
