7d62e6f0a3f5d88624c214118f454fce7093d9d0
hadesyi <hadesleeq@gmail.com>
2015-12-13 00:28:46 +0900

    String type

---
+++ src/test/java/tk/hadeslee/BJ8LF/Annotations/NameTest.java
@@ -0,0 +1,19 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: NameTest
+ * Date: 2015-12-13
+ * Time: 오전 12:26
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Name(first = "John", last = "Jacobs")
+public class NameTest {
+    @Name(first = "Wally", last = "Inman")
+    public void aMethod() {
+
+    }
+}
