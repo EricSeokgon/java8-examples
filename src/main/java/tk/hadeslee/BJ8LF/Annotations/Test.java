4e16eaa4bc3dc3fc6c82e09523be1dd68c4edcfc
hadesyi <hadesleeq@gmail.com>
2015-12-14 09:48:16 +0900

    An Annotation Type Using Other Annotation Types as Data Type of Its Elements

---
+++ src/main/java/tk/hadeslee/BJ8LF/Annotations/Test.java
@@ -0,0 +1,18 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: Test
+ * Date: 2015-12-14
+ * Time: 오전 9:46
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@Description(name = @Name(first = "John", last = "Jacobs"),
+        version = @Version(major = 1, minor = 2),
+        comments = "Just a test class"
+)
+public class Test {
+    //Code goes here
+}
