e87df03f37c23e3cd7a4f05b4c61ddaa4d8ea3c3
hadesyi <hadesleeq@gmail.com>
2015-12-15 09:25:32 +0900

    An Example of a Top-Level Class

---
+++ src/test/java/tk/hadeslee/BJ8LF/innerclasses/TopLevel.java
@@ -0,0 +1,22 @@
+package tk.hadeslee.BJ8LF.innerclasses;
+
+/**
+ * Project: java8-examples
+ * FileName: TopLevel
+ * Date: 2015-12-15
+ * Time: 오전 9:24
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class TopLevel {
+    private int value = 101;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
