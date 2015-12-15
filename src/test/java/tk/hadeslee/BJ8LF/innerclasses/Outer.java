665b239f651972805b268b622a90285ab3ddf89e
hadesyi <hadesleeq@gmail.com>
2015-12-15 09:26:27 +0900

    An Example of a Top-Level Class

---
+++ src/test/java/tk/hadeslee/BJ8LF/innerclasses/Outer.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.BJ8LF.innerclasses;
+
+/**
+ * Project: java8-examples
+ * FileName: Outer
+ * Date: 2015-12-15
+ * Time: 오전 9:25
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Outer {
+    public class Inner {
+
+    }
+}
