b7ceb340c5a8545f374aedca0589aa49fced08bc
hadesyi <hadesleeq@gmail.com>
2015-12-15 09:04:49 +0900

    Todo Annotation Type with String[] as Its Sole Element

---
+++ src/main/java/tk/hadeslee/BJ8LF/Annotations/ToDo.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.BJ8LF.Annotations;
+
+/**
+ * Project: java8-examples
+ * FileName: ToDo
+ * Date: 2015-12-15
+ * Time: 오전 9:03
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface ToDo {
+    String[] items();
+}
+
+
