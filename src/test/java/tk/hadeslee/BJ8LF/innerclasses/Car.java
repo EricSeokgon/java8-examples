ada21cd9bae4344092dbe99ba6161a8f2084dd0b
hadesyi <hadesleeq@gmail.com>
2015-12-15 09:34:08 +0900

    Tire Is a Member Inner Class of the Car Class

--- src/test/java/tk/hadeslee/BJ8LF/innerclasses/Car.java
+++ src/test/java/tk/hadeslee/BJ8LF/innerclasses/Car.java
@@ -15,6 +15,7 @@ public class Car {
 
     //A member inner class named Tire
     public class Tire {
+
         //A member variable for the Tire class
         private double radius;
 
@@ -22,5 +23,20 @@ public class Car {
         public Tire(double radius) {
             this.radius = radius;
         }
+
+        //A member method for the Tire class
+        public double getRadius() {
+            return radius;
+        }
+    }//member inner class declaration ends here
+
+    //A constructor for the Car class
+    public Car(int year) {
+        this.year = year;
+    }
+
+    //A member method for the Car class
+    public int getYear() {
+        return year;
     }
 }
