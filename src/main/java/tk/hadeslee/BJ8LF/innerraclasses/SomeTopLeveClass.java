8ed42719e0a3ec823855c18e4573873c6f14f77c
hadesyi <hadesleeq@gmail.com>
2015-12-18 09:28:32 +0900

    someTopLeveClass is a top-level class

---
+++ src/main/java/tk/hadeslee/BJ8LF/innerraclasses/SomeTopLeveClass.java
@@ -0,0 +1,18 @@
+package tk.hadeslee.BJ8LF.innerraclasses;
+
+/**
+ * Project: java8-examples
+ * FileName: SomeTopLeveClass
+ * Date: 2015-12-18
+ * Time: 오전 9:21
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class SomeTopLeveClass {
+    public void someMethod() {
+        class SomeLovalInnerClass {
+
+        }
+    }
+}
