311cbbd24ac0bd426a46f26c515f1ee78300de87
hadesyi <hadesleeq@gmail.com>
2015-12-22 09:04:42 +0900

    A static member class is not an inner class

---
+++ src/main/java/tk/hadeslee/BJ8LF/innerraclasses/A.java
@@ -0,0 +1,17 @@
+package tk.hadeslee.BJ8LF.innerraclasses;
+
+/**
+ * Project: java8-examples
+ * FileName: A
+ * Date: 2015-12-22
+ * Time: 오전 9:03
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class A {
+    //Static member class
+    public static class B {
+        //Body for class B goes here
+    }
+}
