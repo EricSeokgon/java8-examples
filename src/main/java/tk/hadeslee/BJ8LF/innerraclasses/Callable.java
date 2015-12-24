286fccea21257c63ccc708897891f3c6a3ae355a
hadesyi <hadesleeq@gmail.com>
2015-12-24 10:36:49 +0900

    A callable interface to Implement a callback mechanism

---
+++ src/main/java/tk/hadeslee/BJ8LF/innerraclasses/Callable.java
@@ -0,0 +1,14 @@
+package tk.hadeslee.BJ8LF.innerraclasses;
+
+/**
+ * Project: java8-examples
+ * FileName: Callable
+ * Date: 2015-12-24
+ * Time: 오전 10:35
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Callable {
+    void call();
+}
