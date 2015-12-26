69dafe27c986807a6c2ebb20e302cdaed6108b11
hadesyi <hadesleeq@gmail.com>
2015-12-27 00:27:49 +0900

    reflecting on a class

---
+++ src/main/java/tk/hadeslee/BJ8LF/reflection/FieldReflection.java
@@ -0,0 +1,66 @@
+package tk.hadeslee.BJ8LF.reflection;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
+
+
+/**
+ * Project: java8-examples
+ * FileName: FieldReflection
+ * Date: 2015-12-27
+ * Time: 오전 12:00
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class FieldReflection {
+    public static void main(String[] args) {
+        Class<Person> c = Person.class;
+
+        ArrayList<String> fieldsDesciption = getDeclaredfieldsList(c);
+
+        System.out.println("Declareds Fields for " + c.getName());
+        for (String desc : fieldsDesciption) {
+            System.out.println(desc);
+        }
+
+        fieldsDesciption = getFieldsList(c);
+
+        System.out.println("\nAccessible Fields for " + c.getName());
+        for (String desc : fieldsDesciption) {
+            System.out.println(desc);
+        }
+    }
+
+    private static ArrayList<String> getDeclaredfieldsList(Class c) {
+        Field[] fields = c.getDeclaredFields();
+        ArrayList<String> fieldsList = getFieldsDesciption(fields);
+        return fieldsList;
+    }
+
+    private static ArrayList<String> getFieldsList(Class c) {
+        Field[] fields = c.getFields();
+        ArrayList<String> fieldsList = getFieldsDesciption(fields);
+        return fieldsList;
+    }
+
+    public static ArrayList<String> getFieldsDesciption(Field[] fields) {
+        ArrayList<String> fieldList = new ArrayList<>();
+        for (Field f : fields) {
+            int mod = f.getModifiers() & Modifier.fieldModifiers();
+            String modifiers = Modifier.toString(mod);
+
+            Class<?> type = f.getType();
+            String typeName = type.getTypeName();
+
+            String fieldName = f.getName();
+
+            fieldList.add(modifiers + " " + typeName + " " + fieldName);
+        }
+        return fieldList;
+    }
+
+}
