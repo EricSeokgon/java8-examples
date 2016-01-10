1f519054b443a04004003ebed56c5618149c0682
hadesyi <hadesleeq@gmail.com>
2016-01-10 11:24:41 +0900

    Reflecting on Arrays

--- src/main/java/tk/hadeslee/BJ8LF/reflection/ArrayReflection.java
+++ src/main/java/tk/hadeslee/BJ8LF/reflection/ArrayReflection.java
@@ -14,16 +14,16 @@ import java.lang.reflect.Array;
 public class ArrayReflection {
     public static void main(String[] args) {
         try {
-// Create the array of int of length 2
+            // Create the array of int of length 2
             Object arrayObject = Array.newInstance(int.class, 2);
-// Print the values in array element. Default values will be zero
+            // Print the values in array element. Default values will be zero
             int n1 = Array.getInt(arrayObject, 0);
             int n2 = Array.getInt(arrayObject, 1);
             System.out.println("n1 = " + n1 + ", n2=" + n2);
-// Set the values
+            // Set the values
             Array.set(arrayObject, 0, 101);
             Array.set(arrayObject, 1, 102);
-// Print the values in array element again
+            // Print the values in array element again
             n1 = Array.getInt(arrayObject, 0);
             n2 = Array.getInt(arrayObject, 1);
             System.out.println("n1 = " + n1 + ", n2=" + n2);
