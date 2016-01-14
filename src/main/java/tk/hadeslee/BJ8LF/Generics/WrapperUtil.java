06b8b76a799c2487e84654848ed1b1b95395f9d5
hadesyi <hadesleeq@gmail.com>
2016-01-14 09:38:18 +0900

    Using a Type Parameter to Define a Generic Class

---
+++ src/main/java/tk/hadeslee/BJ8LF/Generics/WrapperUtil.java
@@ -0,0 +1,16 @@
+package tk.hadeslee.BJ8LF.Generics;
+
+/**
+ * Project: java8-examples
+ * FileName: WrapperUtil
+ * Date: 2016-01-14
+ * Time: 오전 9:38
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class WrapperUtil {
+    public static void printDetails(Wrapper<Object> wrapper) {
+// More code goes here
+    }
+}
