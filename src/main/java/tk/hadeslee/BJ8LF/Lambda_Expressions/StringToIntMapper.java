f7247ab2e3a20d2f61c5f43ed8209c45f66bde7a
hadesyi <hadesleeq@gmail.com>
2016-01-16 06:56:47 +0900

    Consider the lambda expression that takes a String parameter and returns its length

---
+++ src/main/java/tk/hadeslee/BJ8LF/Lambda_Expressions/StringToIntMapper.java
@@ -0,0 +1,15 @@
+package tk.hadeslee.BJ8LF.Lambda_Expressions;
+
+/**
+ * Project: java8-examples
+ * FileName: StringToIntMapper
+ * Date: 2016-01-16
+ * Time: 오전 6:55
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@FunctionalInterface
+public interface StringToIntMapper {
+    int map(String str);
+}
