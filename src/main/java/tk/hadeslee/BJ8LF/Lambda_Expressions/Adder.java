920ba922dca5d43522fe8a12415a1a361f22171a
hadesyi <hadesleeq@gmail.com>
2016-01-17 00:19:48 +0900

    A Functional Interface Named Adder

---
+++ src/main/java/tk/hadeslee/BJ8LF/Lambda_Expressions/Adder.java
@@ -0,0 +1,15 @@
+package tk.hadeslee.BJ8LF.Lambda_Expressions;
+
+/**
+ * Project: java8-examples
+ * FileName: Adder
+ * Date: 2016-01-17
+ * Time: 오전 12:19
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@FunctionalInterface
+public interface Adder {
+    double add(double n1, double n2);
+}
