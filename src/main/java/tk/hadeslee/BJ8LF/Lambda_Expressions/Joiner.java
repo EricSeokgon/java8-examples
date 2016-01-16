3c0abb5cd67c3bd95df8c5ce2a207de1495b693a
hadesyi <hadesleeq@gmail.com>
2016-01-17 00:20:26 +0900

    A Functional Interface Named Adder

---
+++ src/main/java/tk/hadeslee/BJ8LF/Lambda_Expressions/Joiner.java
@@ -0,0 +1,15 @@
+package tk.hadeslee.BJ8LF.Lambda_Expressions;
+
+/**
+ * Project: java8-examples
+ * FileName: Joiner
+ * Date: 2016-01-17
+ * Time: 오전 12:20
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@FunctionalInterface
+public interface Joiner {
+    String join(String s1, String s2);
+}
